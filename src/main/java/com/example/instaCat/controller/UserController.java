b108cc4ff1d2953a93d263be6d9150ba39367c2d
Sergei <cot140194@mail.ru>
2021-11-24 13:04:27 +0300

    Added Facade and Controller layer

---
+++ src/main/java/com/example/instaCat/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.example.instaCat.controller;
+
+import com.example.instaCat.dto.UserDTO;
+import com.example.instaCat.entity.User;
+import com.example.instaCat.facade.UserFacade;
+import com.example.instaCat.service.UserService;
+import com.example.instaCat.validators.ResponseErrorValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin //про аннотацию: https://russianblogs.com/article/9354767831/
+public class UserController {
+
+    @Autowired
+    private UserFacade userFacade;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ResponseErrorValidator responseErrorValidator;
+
+    @GetMapping("/")
+    public ResponseEntity<UserDTO> getCurrentUser(Principal principal) {
+        User user = userService.getCurrentUser(principal);
+        UserDTO userDTO = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserProfile(@PathVariable("userId") String userId) {
+        User user = userService.getUserById(Long.parseLong(userId));
+        UserDTO userDTO = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Object> updateUser(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult, Principal principal) {
+        ResponseEntity<Object> listErrors = responseErrorValidator.mappedValidatorService(bindingResult);
+        if (!ObjectUtils.isEmpty(listErrors)) return listErrors;
+
+        User user = userService.updateUser(userDTO, principal);
+        UserDTO userUpdate = userFacade.userToUserDTO(user);
+
+        return new ResponseEntity<>(userUpdate, HttpStatus.OK);
+    }
+}
--- src/main/java/com/example/instaCat/dto/UserDTO.java
+++ src/main/java/com/example/instaCat/dto/UserDTO.java
@@ -7,9 +7,14 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class UserDTO {
     private Long id;
+
     @NotEmpty
     private String firstname;
+
     @NotEmpty
     private String lastname;
+
+    private String username;
+
     private String info;
 }
